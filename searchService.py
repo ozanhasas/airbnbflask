1db54acde37cc60957dea3d4c94fab3582800750
Ozan Hasas <arifozanhasas@gmail.com>
2022-06-22 15:54:25 +0300

    done

--- searchService.py
+++ searchService.py
@@ -1,9 +1,7 @@
 import pymongo
 from flask import Flask, request, jsonify, make_response
 from configparser import ConfigParser
-import pandas as pd
 from bson.objectid import ObjectId
-import json
 import datetime
 from flask_cors import CORS
 import bson.json_util as json_util
@@ -16,6 +14,11 @@ host = config['HOST']['host']
 port = config['HOST']['port']
 username = config['CREDENTIALS']['username']
 password = config['CREDENTIALS']['password']
+
+# start_date_input = input_json['start_date']
+# end_date_input = input_json['end_date']
+# start_date = datetime.datetime(start_date_input['year'], start_date_input['month'], start_date_input['day'])
+# end_date = datetime.datetime(end_date_input['year'], end_date_input['month'], end_date_input['day'])
 
 client = pymongo.MongoClient("mongodb+srv://"+username+":"+password+"@cluster0.fnvrd.mongodb.net/?retryWrites=true&w=majority")
 mydb = client["Airbnb"]
@@ -32,65 +35,111 @@ def getDateHouses():
     end_date = datetime.datetime(end_date_input['year'], end_date_input['month'], end_date_input['day'])
     houses = house_collection.find()
     houses_id_list = []
-    houses_list = []
     reservation_list = []
-
     for i in houses:
-        new_dict = {"_id": i['_id'], "sahipID": i['sahipID'], "price": i['price'], "adres": i['adres'], "sehir": i['sehir'], "long": i['long'], "lat": i['lat'], "image_url": i['image_url'],
-                    "desc": i['desc'], "title": i['title']}
-        houses_list.append(new_dict)
-
-    for a in houses_list:
-        houses_id_list.append(a['_id'])
-
+        houses_id_list.append(i['_id'])
     reservations = reservation_collection.find()
     for i in reservations:
-        new_dict1 = {"_id": i['_id'], "start-date": i['start-date'], "end-date": i['end-date'], "home_id": i['home_id'], "user_id": i['user_id']}
-        reservation_list.append(new_dict1)
-
+        reservation_list.append(i)
     for id in houses_id_list:
         for res in reservation_list:
-            print(type(id))
-            print(type(res['home_id']))
-            if (id == ObjectId(res['home_id'])) & ( ((start_date >= res['start-date']) & (end_date <= res['end-date'])) | ((start_date <= res['start-date']) & (end_date >= res['end-date'])) ):
-                houses_id_list.remove(id)
-
+            if res['home_id'] == '':
+                continue
+            res_home_id = ObjectId(res['home_id'])
+            res_start_date = res['start-date']
+            res_end_date = res['end-date']
+            if id == res_home_id:
+                if (res_end_date > start_date >= res_start_date) or (res_end_date >= end_date > res_start_date) or ((start_date <= res_start_date) and (end_date >= res_end_date)):
+                    houses_id_list.remove(id)
     selected_houses = house_collection.find({"_id": {"$in": houses_id_list}})
-
     house_list_new = []
     for i in selected_houses:
         house_list_new.append(i)
-
     output_json = json_util.dumps(house_list_new)
+    return output_json
+
+
+@app.route('/getHousesByDate')
+def getDateHouses2():
+    input_json = request.get_json()
+    start_date_input = input_json['start_date']
+    end_date_input = input_json['end_date']
+    start_date = datetime.datetime(start_date_input['year'], start_date_input['month'], start_date_input['day'])
+    end_date = datetime.datetime(end_date_input['year'], end_date_input['month'], end_date_input['day'])
+    house_list = []
+    reservations = reservation_collection.find()
+    removed_list = []
+    for res in reservations:
+        if res['home_id'] == '':
+            continue
+        res_home_id = ObjectId(res['home_id'])
+        res_start_date = res['start-date']
+        res_end_date = res['end-date']
+        if not((start_date >= res_end_date) or (res_start_date >= end_date)):
+            removed_list.append(res_home_id)
+    selected_houses = house_collection.find({"_id": {"$nin": removed_list}})
+    for i in selected_houses:
+        house_list.append(i)
+        print(i['_id'])
+    output_json = json_util.dumps(house_list, ensure_ascii=False)
+    return output_json
+
+
+@app.route('/getHousesByDate3', methods=['GET', 'POST'])
+def getDateHouses3():
+    if request.mimetype != 'application/json':
+        args = request.args
+        start_date = datetime.datetime(int(args.get('syear')), int(args.get('smonth')), int(args.get('sday')))
+        end_date = datetime.datetime(int(args.get('eyear')), int(args.get('emonth')), int(args.get('eday')))
+    else:
+        input_json = request.get_json()
+        start_date_input = input_json['start_date']
+        end_date_input = input_json['end_date']
+        start_date = datetime.datetime(start_date_input['year'], start_date_input['month'], start_date_input['day'])
+        end_date = datetime.datetime(end_date_input['year'], end_date_input['month'], end_date_input['day'])
+    house_list = []
+    reservations = reservation_collection.find()
+    removed_list = []
+    for res in reservations:
+        if res['home_id'] == '':
+            continue
+        res_home_id = ObjectId(res['home_id'])
+        res_start_date = res['start-date']
+        res_end_date = res['end-date']
+        if not((start_date >= res_end_date) or (res_start_date >= end_date)):
+            removed_list.append(res_home_id)
+    selected_houses = house_collection.find({"_id": {"$nin": removed_list}})
+    for i in selected_houses:
+        house_list.append(i)
+    output_json = json_util.dumps(house_list, ensure_ascii=False)
+    print(output_json)
     return output_json
 
 
 @app.route('/getHousesByDesc', methods=['POST'])
 def getHousesByDesc():
+
     input_json = request.get_json()
     keyword = input_json['keyword']
-    myquery = {"desc": keyword}
-    houses = house_collection.find(myquery)
+    search_list = [{"desc": {"$regex": ".*" + keyword + ".*"}}]
+    houses = house_collection.find({"$or": search_list})
     houses_list = []
     for i in houses:
-        new_dict = {"_id": i['_id'], "sahipID": i['sahipID'], "price": i['price'], "adres": i['adres'], "sehir": i['sehir'], "long": i['long'], "lat": i['lat'], "image_url": i['image_url'],
-                    "desc": i['desc'], "title": i['title']}
-        houses_list.append(new_dict)
+        houses_list.append(i)
     output_json = json_util.dumps(houses_list)
     return output_json
 
 
 @app.route('/getHousesByTitle')
 def getHousesByTitle():
+
     input_json = request.get_json()
     keyword = input_json['keyword']
-    myquery = {"title": keyword}
-    houses = house_collection.find(myquery)
+    search_list = [{"title": {"$regex": ".*" + keyword + ".*"}}]
+    houses = house_collection.find({"$or": search_list})
     houses_list = []
     for i in houses:
-        new_dict = {"_id": i['_id'], "sahipID": i['sahipID'], "price": i['price'], "adres": i['adres'], "sehir": i['sehir'], "long": i['long'], "lat": i['lat'], "image_url": i['image_url'],
-                    "desc": i['desc'], "title": i['title']}
-        houses_list.append(new_dict)
+        houses_list.append(i)
     output_json = json_util.dumps(houses_list)
     return output_json
 
@@ -99,15 +148,12 @@ def getHousesByTitle():
 def getHousesByCity():
     input_json = request.get_json()
     keyword = input_json['keyword']
-    myquery = {"sehir": keyword}
-    houses = house_collection.find(myquery)
+    search_list = [{"sehir": {"$regex": ".*" + keyword + ".*"}}]
+    houses = house_collection.find({"$or": search_list})
     houses_list = []
     for i in houses:
-        new_dict = {"_id": i['_id'], "sahipID": i['sahipID'], "price": i['price'], "adres": i['adres'], "sehir": i['sehir'], "long": i['long'], "lat": i['lat'], "image_url": i['image_url'],
-                    "desc": i['desc'], "title": i['title']}
-        houses_list.append(new_dict)
+        houses_list.append(i)
     output_json = json_util.dumps(houses_list)
-
     return output_json
 
 
@@ -115,13 +161,7 @@ def getHousesByCity():
 def gethouses():
 
     input_json = request.get_json()
-    #start_date_input = input_json['start_date']
-    #end_date_input = input_json['end_date']
     keyword = input_json['keyword']
-    #start_date = datetime.datetime(start_date_input['year'], start_date_input['month'], start_date_input['day'])
-    #end_date = datetime.datetime(end_date_input['year'], end_date_input['month'], end_date_input['day'])
-
-
     search_list = [{"desc": {"$regex": ".*"+keyword+".*"}}, {"title": {"$regex": ".*"+keyword+".*"}}, {"sehir": {"$regex": ".*"+keyword+".*"}}]
     houses = house_collection.find({"$or": search_list})
     houses_list = []
